07c4b89653c18ad09879c24428c03ab3aa8aa9ca
YuriiSokol <yura357117@gmail.com>
2023-04-05 18:35:32 +0200

    q

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = document.querySelector('.gallery');
 galleryItems.forEach(item => {
